ffe1cd9a3eafbc17fd94247d9b636d150b89ae89
Peter Dell <peter.dell@web.de>
2026-01-03 02:20:50 +0100

    Adapt Patch-16 filenames

--- asm/Patch-16/build.cmd
+++ asm/Patch-16/build.cmd
@@ -1,5 +1,6 @@
 @echo off
 if not exist out\ (mkdir out)
-mads -hc:out/tracker_obx.h -l:out/tracker_obx.lst -o:out/tracker.obx rmtplayr.a65
+mads -hc:out\tracker_obx.h -l:out\tracker_obx.lst -o:out\tracker.obx rmtplayr.a65
+rem mads -o:out\rmtplayer.xex rmtplayer.a65
 pause
 
