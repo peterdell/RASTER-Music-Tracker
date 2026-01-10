776cc442826336676138bdd7f734136be757a627
Vin Samuel <vinscool@live.ca>
2026-01-10 16:53:45 -0500

    Fix Stuck CTRL Key Bug
    This was meant to be set to FALSE on Key Up.

--- src/RmtView.cpp
+++ src/RmtView.cpp
@@ -1855,7 +1855,7 @@ void CRmtView::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
     else
         if (nChar == VK_CONTROL)
         {
-            g_controlkey = TRUE;
+            g_controlkey = FALSE;
         }
         else
             if (nChar == VK_LMENU)
