7b83c86e6c2df2e19cd19e87eed68e6baf5c6ff7
Peter Dell <peter.dell@web.de>
2026-01-18 16:17:28 +0100

    Reduce pre-build log output

--- build/build_rmt_pre.bat
+++ build/build_rmt_pre.bat
@@ -1,10 +1,12 @@
+@echo off
+
 rem This is called in the "src" folder.
 rem The first parameter is the output directory.
 
 rem Prepare template folder with the most recent contents.
-xcopy ..\doc\*.html  ..\rmt\docs
-xcopy ..\doc\*.gif   ..\rmt\docs
+xcopy /Y ..\doc\*.html  ..\rmt\docs
+xcopy /Y ..\doc\*.gif   ..\rmt\docs
 
 rem Clean the output folder completely.
-del /Q /S %1
+del /Q /S %1 >nul 
 
