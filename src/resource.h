7159b4243d5ccd82ace53c05ada7a5760dc20425
Peter Dell <peter.dell@web.de>
2026-02-06 00:40:50 +0100

    Fix: Shift+F6 for ID_BLOCK_PLAY_AND_LOOP

--- src/resource.h
+++ src/resource.h
@@ -362,7 +362,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        205
-#define _APS_NEXT_COMMAND_VALUE         32920
+#define _APS_NEXT_COMMAND_VALUE         32922
 #define _APS_NEXT_CONTROL_VALUE         1198
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
